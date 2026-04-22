d55fc8afe02bb20e54ebfdd2cab76c009159e371
root <root@serobq79b996f.local>
2026-04-22 18:04:09 +0000

    feat: add openchamber image build (Dockerfile + config + CI workflow)

---
+++ images/openchamber/config.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+SOURCE_REPO="https://github.com/btriapitsyn/openchamber.git"
+SOURCE_REF="latest-release"
+IMAGE_SIZE=2048
+TAG="openchamber-rootfs:latest"
+OUTPUT="openchamber-rootfs.ext4"
+FTP_SUBDIR="openchamber"
