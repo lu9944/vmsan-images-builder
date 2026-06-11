d61e9499c80f41210e3e397251962d150cfc7650
Ubuntu <ubuntu@localhost.localdomain>
2026-06-11 11:41:40 +0800

    feat: add new-api image build

---
+++ images/new-api/config.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+SOURCE_REPO="https://github.com/QuantumNous/new-api.git"
+SOURCE_REF="latest-release"
+IMAGE_SIZE=2048
+TAG="new-api-rootfs:latest"
+OUTPUT="new-api-rootfs.ext4"
