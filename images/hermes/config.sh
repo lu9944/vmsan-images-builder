3b5b829a0a4877b55c2ea24e9209f5da064e0967
Ubuntu <ubuntu@localhost.localdomain>
2026-06-18 15:55:43 +0800

    feat: add hermes-agent image build

---
+++ images/hermes/config.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# hermes-agent (NousResearch) vmsan rootfs build config.
+SOURCE_REPO="https://github.com/NousResearch/hermes-agent.git"
+SOURCE_REF="main"
+IMAGE_SIZE=4096
+TAG="hermes-rootfs:latest"
+OUTPUT="hermes-rootfs.ext4"
